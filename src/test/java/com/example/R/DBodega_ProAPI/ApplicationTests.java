ce7b4756af6aa5aa074081cac37b43626ac480a6
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-09-04 22:20:28 -0600

    Subi proyecto Spring Boot completo

---
+++ src/test/java/com/example/R/DBodega_ProAPI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.R.DBodega_ProAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
