c88bc3f04b67e3747ad9cbcd1c7c544e7e53bf85
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-09-07 20:53:44 -0600

    Se crearo la interfaz de repositorio  de categoria

---
+++ src/main/java/com/example/R/DBodega_ProAPI/repositorios/ICategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.example.R.DBodega_ProAPI.repositorios;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.R.DBodega_ProAPI.modelos.Categoria;
+
+public interface ICategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+        Page<Categoria> findByNombreContainingIgnoreCaseOrderByIdDesc(String nombre, Pageable pageable);
+
+}
