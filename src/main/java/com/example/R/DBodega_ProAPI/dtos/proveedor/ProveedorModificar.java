e59a0fa07c63ec8f7fee06c6eaaa6a21a6dfb0fa
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-09-09 10:05:18 -0600

    Cree la carpeta proveedor dentro de la carpeta dtos y cree los archivos ProveedorGuardar, ProveedorSalida y ProveedorModificar

---
+++ src/main/java/com/example/R/DBodega_ProAPI/dtos/proveedor/ProveedorModificar.java
@@ -0,0 +1,16 @@
+package com.example.R.DBodega_ProAPI.dtos.proveedor;
+
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProveedorModificar implements Serializable{
+    private Integer id;
+    private String nombre;
+    private String nombreEmpresa;
+    private String telefono;
+    private String email;
+    private String direccion;    
+}
