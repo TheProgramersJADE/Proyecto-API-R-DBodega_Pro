fef6bd38ae88f5ff367145af3fa7c2dfbe84b6eb
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-09-09 08:17:35 -0600

    Se creo el modelo de tipo de movimientos con su funcionalidad

---
+++ src/main/java/com/example/R/DBodega_ProAPI/modelos/TipoMovimiento.java
@@ -0,0 +1,35 @@
+package com.example.R.DBodega_ProAPI.modelos;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tipoMovimientos")
+public class TipoMovimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre", length = 100, nullable = false)
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 100, message = "El nombre no puede superar los 100 caracteres")
+    private String nombre;
+
+    @Column(name = "editar_costo", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    @NotNull(message = "El valor de editar_costo es obligatorio")
+    private Boolean editarCosto;
+
+    @Column(name = "tipo", nullable = false)
+    @NotNull(message = "El valor de tipo es obligatorio")
+    private Integer tipo;
+
+}
