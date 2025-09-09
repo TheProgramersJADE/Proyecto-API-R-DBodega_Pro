c63acb7a29dbaa21e47048252b052392ec95ebe5
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-09-09 10:31:31 -0600

    Cree el repositorio de Proveedor

---
+++ src/main/java/com/example/R/DBodega_ProAPI/repositorios/IProveedorRepository.java
@@ -0,0 +1,11 @@
+package com.example.R.DBodega_ProAPI.repositorios;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.R.DBodega_ProAPI.modelos.Proveedor;
+
+public interface IProveedorRepository extends JpaRepository<Proveedor, Integer> {
+    Page<Proveedor> findByNombreContainingIgnoreCaseAndNombreEmpresaContainingIgnoreCaseOrderByIdDesc(String nombre, String nombreEmpresa, Pageable pageable);
+}
