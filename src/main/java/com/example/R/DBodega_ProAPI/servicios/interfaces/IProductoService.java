39f5026b1377bdedfb1a4cf460ddb0488e8c458f
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-09-12 10:38:36 -0600

    Cree el archivo IProductoService dentro de la carpete Interfaces de servicios

---
+++ src/main/java/com/example/R/DBodega_ProAPI/servicios/interfaces/IProductoService.java
@@ -0,0 +1,27 @@
+package com.example.R.DBodega_ProAPI.servicios.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.example.R.DBodega_ProAPI.dtos.producto.ProductoGuardar;
+import com.example.R.DBodega_ProAPI.dtos.producto.ProductoModificar;
+import com.example.R.DBodega_ProAPI.dtos.producto.ProductoSalida;
+import com.example.R.DBodega_ProAPI.modelos.Producto;
+
+public interface IProductoService {
+List<ProductoSalida> obtenerTodos();
+
+    Page<ProductoSalida> obtenerTodosPaginados(Pageable pageable);
+
+    ProductoSalida obtenerPorId(Integer id);
+
+    ProductoSalida crear(ProductoGuardar productoGuardar);
+
+    ProductoSalida editar(ProductoModificar ProductoModificar);
+
+    void eliminarPorId(Integer id);
+
+    Page<ProductoSalida> findByNombreContainingIgnoreCaseAndCategoriaNombreContainingIgnoreCaseAndProveedorNombreContainingIgnoreCaseOrderByIdDesc(String nombre, String categoriaNombre, String proveedorNombre, Pageable pageable);
+    Optional<Producto> findByNombre(String nombre);
+}
