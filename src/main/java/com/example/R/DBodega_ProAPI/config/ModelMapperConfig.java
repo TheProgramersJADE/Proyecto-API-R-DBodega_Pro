17467b5ef2311a468a78ec0b4d8ac30a64a10a33
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-09-14 15:39:14 -0600

    Se eliminaron unos archivos y se arreglo el model mapper

--- src/main/java/com/example/R/DBodega_ProAPI/config/ModelMapperConfig.java
+++ src/main/java/com/example/R/DBodega_ProAPI/config/ModelMapperConfig.java
@@ -3,28 +3,13 @@ package com.example.R.DBodega_ProAPI.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.example.R.DBodega_ProAPI.modelos.Producto;
-import com.example.R.DBodega_ProAPI.dtos.producto.ProductoSalida;
+
 
 @Configuration
 public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper mapper = new ModelMapper();
-
-        // Configuración global
-        mapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
-
-        // Mapear Producto -> ProductoSalida sin campos calculados
-        mapper.typeMap(Producto.class, ProductoSalida.class)
-                .addMappings(m -> {
-                    m.skip(ProductoSalida::setEstadoStock);
-                    m.skip(ProductoSalida::setEstadoStockClass);
-                });
-
-        return mapper;
+        return new ModelMapper();
     }
 }
