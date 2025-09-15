7a3d86be2f141cf92a13ea02f778281ea9ba0699
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-09-14 22:15:32 -0600

    Cree el archivo IMovimientoEntradaSalidaServide de Interfaces de Servicios y Modifique el Repositorio de MovimientoEntradaSalida

--- src/main/java/com/example/R/DBodega_ProAPI/repositorios/IMovimientoEntradaSalidaRepository.java
+++ src/main/java/com/example/R/DBodega_ProAPI/repositorios/IMovimientoEntradaSalidaRepository.java
@@ -5,10 +5,9 @@ import java.util.List;
 import org.springdoc.core.converters.models.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.R.DBodega_ProAPI.modelos.Categoria;
 import com.example.R.DBodega_ProAPI.modelos.MovimientoEntradaSalida;
 
-public interface IMovimientoEntradaSalidaRepository extends JpaRepository<Categoria, Integer> {
+public interface IMovimientoEntradaSalidaRepository extends JpaRepository<MovimientoEntradaSalida, Integer> {
     Page<MovimientoEntradaSalida> findByProductoNombreContainingIgnoreCaseAndTipoMovimientoNombreOrderByIdDesc(
             String nombreProducto,
             String nombre,
---
+++ src/main/java/com/example/R/DBodega_ProAPI/servicios/interfaces/IMovimientoEntradaSalidaService.java
@@ -0,0 +1,27 @@
+package com.example.R.DBodega_ProAPI.servicios.interfaces;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.example.R.DBodega_ProAPI.dtos.movimientoEntradaSalida.MovimientoEntradaSalidaGuardar;
+import com.example.R.DBodega_ProAPI.dtos.movimientoEntradaSalida.MovimientoEntradaSalidaModificar;
+import com.example.R.DBodega_ProAPI.dtos.movimientoEntradaSalida.MovimientoEntradaSalidaSalida;
+import com.example.R.DBodega_ProAPI.modelos.MovimientoEntradaSalida;
+
+public interface IMovimientoEntradaSalidaService {
+    List<MovimientoEntradaSalidaSalida> obtenerTodos();
+
+    Page<MovimientoEntradaSalidaSalida> obtenerTodosPaginados(Pageable pageable);
+
+    MovimientoEntradaSalidaSalida obtenerPorId(Integer id);
+
+    MovimientoEntradaSalidaSalida crear(MovimientoEntradaSalidaGuardar movimientoEntradaSalidaGuardar);
+
+    MovimientoEntradaSalidaSalida editar(MovimientoEntradaSalidaModificar movimientoEntradaSalidaModificar);
+
+    Page<MovimientoEntradaSalidaSalida> findByProductoNombreContainingIgnoreCaseAndTipoMovimientoNombreOrderByIdDesc(
+            String nombreProducto, String nombre, Pageable pageable);
+
+    List<MovimientoEntradaSalida> findByFechaBetween(LocalDateTime inicio, LocalDateTime fin);
+}
